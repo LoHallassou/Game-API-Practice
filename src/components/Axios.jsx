1c5d73169af52c4ac20649500a5bb10dd9c21439
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-03-19 20:21:42 +0000

    Adding search game function

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -1,25 +1,26 @@
-import { useState, useEffect } from "react";
+
 import axios from 'axios';
 
-const axios = () => {
-  const [games, setGames] = useState([])
-  useEffect(() => {
-    axios.get("https://api.igdb.com/v4/games")
-        .then((res) => {
-            setGames(res.data)
-        })
-        .then((data)=>{
+const API_KEY = '2e34e67511c14a3d880db20cf0570831';
+const BASE_URL = 'https://api.rawg.io/api';
 
-        })
-      }, [])
-    }
+export const fetchGames = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/games?key=${API_KEY}`);
+    console.log("res:",response.data)
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching games:', error);
+    throw error;
+  }
+};
 
-return(
-  <div>
-    {games.map((game) => ()
-    )
-
-    }
-  </div>
- 
-)
+export const fetchGameDetails = async (gameId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/games/${gameId}?key=${API_KEY}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching game details:', error);
+    throw error;
+  }
+};
--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,1 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import { fetchGames } from '../components/Axios';
 
+const GameList = () => {
+    const [games, setGames] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [selectedGame, setSelectedGame] = useState(null);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const data = await fetchGames();
+          console.log("data:",data);
+          // setGames(data.results);
+          const gameResults = data.results;
+          setData(gameResults);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    const handleSearch = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${BASE_URL}/games?search=${searchTerm}&key=${API_KEY}`);
+        const data = await response.json();
+        //setGames(data.results);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error searching games:', error);
+        setLoading(false);
+      }
+    };
+  
+    const handleGameSelect = async (id) => {
+      try {
+        const gameDetails = await fetchGameDetails(id);
+        setSelectedGame(gameDetails);
+      } catch (error) {
+        console.error('Error fetching game details:', error);
+      }
+    };
+  
+    const handleChange = (event) => {
+      setSearchTerm(event.target.value);
+      console.log(event.target.value);
+    };
+  
+    return (
+      <div>
+        <h1>Game List</h1>
+        <input type="text" value={searchTerm} onChange={handleChange} placeholder="Search for a game" />
+        <button onClick={handleSearch}>Search</button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            {data ? (
+            <ul>
+            {data.map((game) => (
+              <li key={game.id}>{game.name}</li>
+            ))}
+          </ul>
+          ) : (
+           <div>No games</div>
+           )}
+            <ul>
+              {games.map((game) => (
+                <li key={game.id} onClick={() => handleGameSelect(game.id)}>{game.name}</li>
+              ))}
+            </ul>
+            {selectedGame && (
+              <div>
+                <h2>{selectedGame.name}</h2>
+                <p>Genres: {selectedGame.genres.map(genre => genre.name).join(', ')}</p>
+              </div>
+           
+            )}
+          </div>
+           
+          
+        )}
+
+      </div>
+    );
+  };
+  
+  export default GameList;
